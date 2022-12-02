6b1405e30567c9b0ef5976f03d9ddc5bc81439c8
Alfarau Ala <alfarau@qlue.id>
2022-12-02 11:03:19 +0700

    first push

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+
+    e2e: {
+
+        baseUrl : 'https://parabank.parasoft.com/parabank/index.htm',
+
+        specPattern : "cypress/support/e2e",
+
+        supportFile : false,
+
+    },
+
+    env: {
+        username : "farau.test",
+        password : "farau123"
+    }
+
+})
